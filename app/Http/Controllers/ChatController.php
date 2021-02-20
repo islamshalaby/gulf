fdad5a0339f3a570a90c9a59c0839e21b8aac197
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-02-20 15:23:52 +0200

    times fix

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -134,10 +134,11 @@ class ChatController extends Controller
 
         $data['messages'] =  Message::where('ad_product_id' , $request->id)
                             ->where('conversation_id',$partic->conversation_id)
-                            ->select('id','message','type','user_id','conversation_id','ad_product_id')
+                            ->select('id','message','type','user_id','conversation_id','ad_product_id','created_at')
                             ->orderBy('id','desc')
                             ->get()
                             ->map(function ($messages) use ($user_id){
+                                $messages->time = $messages->created_at->format('g:i a');
                                 if($messages->user_id == $user_id){
                                     $messages->position = 'right';
                                 }else{
